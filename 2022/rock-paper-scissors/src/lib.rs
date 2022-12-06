f374dd204086cb8ffa5f65e6e062f993dab8127a
ladihzey <ladihzey@proton.me>
2022-12-07 00:05:31 +0300

    Complete day 2 part 2 "Rock Paper Scissors" puzzle

--- 2022/rock-paper-scissors/src/lib.rs
+++ 2022/rock-paper-scissors/src/lib.rs
@@ -1,4 +1,6 @@
 use std::cmp::{Ord, Ordering};
+use Ordering::{Less, Greater, Equal};
+use HandShape::{Rock, Paper, Scissors};
 
 #[derive(PartialEq, Eq, Clone)]
 enum HandShape {
@@ -8,19 +10,19 @@ enum HandShape {
 }
 
 impl HandShape {
-    fn play(&self, other: &Self) -> u32 {
-        match self.cmp(other) {
-            Ordering::Greater => 6,
-            Ordering::Equal => 3,
-            Ordering::Less => 0,
+    fn get_score(&self) -> u32 {
+        match self {
+            Rock => 1,
+            Paper => 2,
+            Scissors => 3,
         }
     }
 
-    fn get_score(&self) -> u32 {
-        match self {
-            HandShape::Rock => 1,
-            HandShape::Paper => 2,
-            HandShape::Scissors => 3,
+    fn play(&self, other: &Self) -> u32 {
+        match self.cmp(other) {
+            Greater => 6,
+            Equal => 3,
+            Less => 0,
         }
     }
 }
@@ -33,54 +35,73 @@ impl PartialOrd for HandShape {
 impl Ord for HandShape {
     fn cmp(&self, other: &Self) -> Ordering {
         match (self, other) {
-            (HandShape::Rock, HandShape::Paper) => Ordering::Less,
-            (HandShape::Rock, HandShape::Scissors) => Ordering::Greater,
-            (HandShape::Paper, HandShape::Rock) => Ordering::Greater,
-            (HandShape::Paper, HandShape::Scissors) => Ordering::Less,
-            (HandShape::Scissors, HandShape::Rock) => Ordering::Less,
-            (HandShape::Scissors, HandShape::Paper) => Ordering::Greater,
-            _ => Ordering::Equal
+            (Rock, Paper) => Less,
+            (Rock, Scissors) => Greater,
+            (Paper, Rock) => Greater,
+            (Paper, Scissors) => Less,
+            (Scissors, Rock) => Less,
+            (Scissors, Paper) => Greater,
+            _ => Equal
         }
     }
 }
-impl From<&str> for HandShape {
-    fn from(code: &str) -> Self {
-        match code {
-            "A" | "X" => Self::Rock,
-            "B" | "Y" => Self::Paper,
-            "C" | "Z" => Self::Scissors,
-            _ => panic!("Unknown handshape code {}", code)
+
+pub struct GameRound(HandShape, HandShape);
+
+impl GameRound {
+    pub fn parse_by_guess(code_pair: &str) -> Self {
+        let shapes = code_pair
+            .split(' ')
+            .map(|code| match code {
+                "A" | "X" => Rock,
+                "B" | "Y" => Paper,
+                "C" | "Z" => Scissors,
+                _ => panic!("Unknown hand shape code {code}")
+            })
+            .collect::<Vec<HandShape>>();
+
+        Self(shapes[0].clone(), shapes[1].clone())
+    }
+
+    pub fn parse_correctly(code_pair: &str) -> Self {
+        match code_pair {
+            "A X" => Self(Rock, Scissors),
+            "A Y" => Self(Rock, Rock),
+            "A Z" => Self(Rock, Paper),
+            "B X" => Self(Paper, Rock),
+            "B Y" => Self(Paper, Paper),
+            "B Z" => Self(Paper, Scissors),
+            "C X" => Self(Scissors, Paper),
+            "C Y" => Self(Scissors, Scissors),
+            "C Z" => Self(Scissors, Rock),
+            _ => panic!("Unknown hand shape code pair {code_pair}")
         }
+    }
+
+    fn calculate_round_score(&self) -> u32 {
+        let GameRound(first, second) = self;
+        second.get_score() + second.play(first)
     }
 }
 
 pub struct Game {
-    rounds: Vec<(HandShape, HandShape)>,
+    rounds: Vec<GameRound>,
 }
 
 impl Game {
-    pub fn new(round_codes: &str) -> Game {
+    pub fn new(round_codes: &str, parse: &dyn Fn(&str) -> GameRound) -> Self {
         let rounds = round_codes
             .split('\n')
-            .map(Self::parse_round_code)
+            .map(parse)
             .collect();
         
-        Game { rounds }
-    }
-
-    fn parse_round_code(round_code: &str) -> (HandShape, HandShape) {
-        let shapes = round_code
-            .split(' ')
-            .map(|code| code.into())
-            .collect::<Vec<HandShape>>();
-        
-        (shapes[0].clone(), shapes[1].clone())
+        Self { rounds }
     }
 
     pub fn calculate_game_score(&self) -> u32 {
         self.rounds
             .iter()
-            .map(|(first, second)| second.play(first) + second.get_score())
+            .map(|round| round.calculate_round_score())
             .sum()
     }
 }
@@ -89,9 +110,17 @@ impl Game {
 mod tests {
     use super::*;
 
+    const input: &str = "A Y\nB X\nC Z";
+
     #[test]
-    fn should_correctly_calculate_game_score() {
-        let game = Game::new("A Y\nB X\nC Z");
+    fn calculate_game_score_by_guess_strategy() {
+        let game = Game::new(input, &GameRound::parse_by_guess);
         assert_eq!(game.calculate_game_score(), 15);
+    }
+
+    #[test]
+    fn calculate_game_score_by_correct_strategy() {
+        let game = Game::new(input, &GameRound::parse_correctly);
+        assert_eq!(game.calculate_game_score(), 12);
     }
 }
--- 2022/rock-paper-scissors/src/main.rs
+++ 2022/rock-paper-scissors/src/main.rs
@@ -1,9 +1,16 @@
 use std::fs;
-use rock_paper_scissors::Game;
+use rock_paper_scissors::{Game, GameRound};
 
 fn main() {
     let input = fs::read_to_string("assets/input.txt").unwrap();
-    let game_score = Game::new(&input).calculate_game_score();
 
-    println!("The game score is {game_score}");
+    println!(
+        "The game score with guess strategy is {}",
+        Game::new(&input, &GameRound::parse_by_guess).calculate_game_score()
+    );
+
+    println!(
+        "The game with correct strategy is {}",
+        Game::new(&input, &GameRound::parse_correctly).calculate_game_score()
+    );
 }
