bc29ffd8ee4b13fad46da41e1d7f5a5a294c26ca
ladihzey <ladihzey@proton.me>
2022-12-18 02:04:50 +0300

    Use parser combinators instead of regexps for day 4

--- 2022/camp-cleanup/src/lib.rs
+++ 2022/camp-cleanup/src/lib.rs
@@ -1,5 +1,11 @@
-use regex::Regex;
 use std::ops::RangeInclusive;
+use nom::{
+    bytes::complete::tag,
+    character::complete::{self, newline},
+    multi::separated_list1,
+    sequence::separated_pair,
+    *
+};
 
 pub fn count_duplicating_assignments(input: &str) -> usize {
     parse_assignments(input)
@@ -15,12 +21,14 @@ pub fn count_overlapping_assignments(input: &str) -> usize {
         .count()
 }
 
+type AssignmentRange = RangeInclusive<u32>;
+
 trait Intersectionable {
     fn includes(&self, other: &Self) -> bool;
     fn overlaps(&self, other: &Self) -> bool;
 }
 
-impl Intersectionable for RangeInclusive<u32> {
+impl Intersectionable for AssignmentRange {
     fn includes(&self, other: &Self) -> bool {
         self.contains(other.start()) && self.contains(other.end())
     }
@@ -31,24 +39,19 @@ impl Intersectionable for RangeInclusive<u32> {
     }
 }
 
-fn parse_assignments(input: &str) -> Vec<(RangeInclusive<u32>, RangeInclusive<u32>)> {
-    let re = Regex::new(r"(\d+)").unwrap();
+fn assignment_range(input: &str) -> IResult<&str, AssignmentRange> {
+    let (input, (start, end)) = separated_pair(complete::u32, tag("-"), complete::u32)(input)?;
+    Ok((input, start..=end))
+}
 
-    input
-        .lines()
-        .map(|assignments_str| re
-            .captures_iter(assignments_str)
-            .map(|c| c[0].parse::<u32>().unwrap())
-            .collect::<Vec<u32>>()
-        )
-        .map(|assignments| {
-            if let [a_start, a_end, b_start, b_end] = assignments.as_slice() {
-                (*a_start..=*a_end, *b_start..=*b_end)
-            } else {
-                panic!("Can't parse assignments");
-            }
-        })
-        .collect()
+fn assignment_pair(input: &str) -> IResult<&str, (AssignmentRange, AssignmentRange)> {
+    let (input, (left, right)) = separated_pair(assignment_range, tag(","), assignment_range)(input)?;
+    Ok((input, (left, right)))
+}
+
+fn parse_assignments(input: &str) -> Vec<(AssignmentRange, AssignmentRange)> {
+    let (_, ranges) = separated_list1(newline, assignment_pair)(input).unwrap();
+    ranges
 }
 
 #[cfg(test)]
